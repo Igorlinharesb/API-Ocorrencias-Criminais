0f303a39ac1627b2294d1146000785e5d7ffce32
Igor Linhares <igorlinhares@alu.ufc.br>
2020-08-25 15:02:39 -0300

    Modelos feitos

--- models.py
+++ models.py
@@ -5,8 +5,19 @@ db = SQLAlchemy()
 
 class Municipio (db.Model):
     __tablename__ = "municipios"
-
+    uf = db.Column(db.String, primary_key=True)
+    estado = db.Column(db.String)
+    regiao = db.Column(db.String)
+    mes = db.Column(db.Integer)
+    ano = db.Column(db.Integer)
+    vitimas = db.Column(db.Integer)
 
 
 class Estado(db.Model):
     __tablename__ = "estados"
+    uf = db.Column(db.String, primary_key=True)
+    estado = db.Column(db.String)
+    crime = db.Column(db.String)
+    mes = db.Column(db.String)
+    vitimas = db.Column(db.Integer)
+    ocorrencias =db.Column(db.Integer)
