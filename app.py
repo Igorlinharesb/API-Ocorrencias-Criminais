45cecc1c5610def3610577e029592f42c44fcf8a
Fabriciox7 <fabricioldi36@alu.ufc.br>
2020-09-07 19:28:56 -0300

    MunByDataIeIF

--- app.py
+++ app.py
@@ -397,12 +397,106 @@ class MunByUF(Resource):
 
 class MunByDataI(Resource):
     def get(self, data_inicio, page_num):
-        pass
+        try:
+            mes = int(data_inicio.split("-")[0])
+            ano = int(data_inicio.split("-")[1])
+
+            if mes < 1 or mes > 12:
+              return jsonify({"erro": "Data inválida, verifique a documentação da API."}), 422
+
+        except:
+            return jsonify({"erro": "Data inválida, verifique a documentação da API."}), 422
+
+        q1 = Estado.query.filter(Estado.ano == ano).filter(Estado.mes >= mes)
+
+        q2 = Estado.query.filter(Estado.ano > ano)
+
+        e = q1.union_all(q2)
+
+        ests = e.paginate(per_page=100, page=int(page_num))
+
+        results = []
+        for item in ests.items:
+            temp = {"estado": item.estado,
+                    "sigla_UF": item.sigla_UF,
+                    "tipo_crime": item.crime,
+                    "mes_ano": f"{item.mes}-{item.ano}",
+                    "vitimas": item.vitimas,
+                    "ocorrencias": item.ocorrencias}
+
+            results.append(temp)
+
+        num_items = len(results)
+
+        if ests.has_next:
+            next_page = ests.next_num
+        else:
+            next_page = None
+
+        if ests.has_prev:
+            prev_page = ests.prev_num
+        else:
+            prev_page = None
+
+        return jsonify({"has_next": ests.has_next,
+                        "next_page": next_page,
+                        "has_prev": ests.has_prev,
+                        "prev_page": prev_page,
+                        "num_items": num_items,
+                        "total_results": ests.total,
+                        "num_pages": ests.pages,
+                        "results": results})
 
 
 class MunByDataF(Resource):
     def get(self, data_fim, page_num):
-        pass
+        try:
+            mes = int(data_fim.split("-")[0])
+            ano = int(data_fim.split("-")[1])
+
+            if mes < 1 or mes > 12:
+                return jsonify({"erro": "Data inválida, verifique a documentação da API."}), 422
+
+        except:
+            return jsonify({"erro": "Data inválida, verifique a documentação da API."}), 422
+
+        q1 = Municipio.query.filter(Municipio.ano < ano)
+
+        q2 = Municipio.query.filter(Municipio.ano == ano).filter(Municipio.mes <= mes)
+
+        m = q1.union_all(q2)
+
+        muns = m.paginate(per_page=100, page=int(page_num))
+
+        results = []
+        for item in muns.items:
+            temp = {"municipio": item.municipio,
+                    "sigla_UF": item.sigla_UF,
+                    "regiao": item.regiao,
+                    "mes_ano": f"{item.mes}-{item.ano}",
+                    "vitimas": item.vitimas}
+            results.append(temp)
+
+        num_items = len(results)
+
+        if muns.has_next:
+            next_page = muns.next_num
+        else:
+            next_page = None
+
+        if muns.has_prev:
+            prev_page = muns.prev_num
+        else:
+            prev_page = None
+
+        return jsonify({"has_next": muns.has_next,
+                        "next_page": next_page,
+                        "has_prev": muns.has_prev,
+                        "prev_page": prev_page,
+                        "num_items": num_items,
+                        "total_results": muns.total,
+                        "num_pages": muns.pages,
+                        "results": results})
 
 
 class MunByDataIF(Resource):
