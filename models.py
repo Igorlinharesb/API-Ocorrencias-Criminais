6410a743c6503a9e6ba983abfdded924a5ce3dbb
Igor Linhares <igorlinhares@alu.ufc.br>
2020-09-07 01:47:39 -0300

    Rota EstadoByUFDataI concluída e base da dados atualizada

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 
 class Municipio (db.Model):
-    __tablename__ = "municipios"
+    __tablename__ = "municipio"
     id = db.Column(db.Integer, primary_key=True)
     municipio = db.Column(db.String)
     sigla_UF = db.Column(db.String)
@@ -15,38 +15,12 @@ class Municipio (db.Model):
 
 
 class Estado(db.Model):
-    __tablename__ = "estados"
+    __tablename__ = "estado"
     id = db.Column(db.Integer, primary_key=True)
-    estado = db.Column(db.String)
-    sigla_uf = db.String(db.String)
-    crime = db.Column(db.String)
-    mes = db.Column(db.Integer)
-    ano = db.Column(db.Integer)
-    vitimas = db.Column(db.Integer)
-    ocorrencias =db.Column(db.Integer)
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-
-class Municipio (db.Model):
-    __tablename__ = "municipios"
-    id = db.Column(db.Integer, primary_key=True)
-    municipio = db.Column(db.String)
     sigla_UF = db.Column(db.String)
-    regiao = db.Column(db.String)
-    mes = db.Column(db.Integer)
-    ano = db.Column(db.Integer)
-    vitimas = db.Column(db.Integer)
-
-
-class Estado(db.Model):
-    __tablename__ = "estados"
-    id = db.Column(db.Integer, primary_key=True)
     estado = db.Column(db.String)
-    sigla_uf = db.String(db.String)
     crime = db.Column(db.String)
     mes = db.Column(db.Integer)
     ano = db.Column(db.Integer)
     vitimas = db.Column(db.Integer)
-    ocorrencias =db.Column(db.Integer)
+    ocorrencias = db.Column(db.Integer)
